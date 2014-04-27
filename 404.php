ad8a828d9648e5fd29c060102edd1a11257eaa3f
Frank Riccobono <F.Riccobono@gmail.com>
2014-04-27 14:10:01 -0400

    added a working 404 error page

--- 404.php
+++ 404.php
@@ -1,3 +1,24 @@
-<?
-
-?>
+<!doctype html>
+<html>
+   <head>
+      <?php wp_head();?>
+   </head>
+   <body>
+      <?php 
+         get_header();
+         include('slideshow.inc');
+      ?>
+      <div class="tealcor"></div>
+      <div class="teal">
+         a unique organization dedicated to breast cancer research
+         <img src="<?php echo get_stylesheet_directory_uri() ?>/img/ribbon.png" class="noselect"/>
+         <div class="clear noselect">&nbsp;</div>
+      </div>
+   </div>
+   <div id="content_zone">
+      <div class="box b640">
+         <div class='eo' style='width: auto'>We're sorry.  This page could not be found.</div>
+      </div>
+   </div>
+</body>
+</html>
