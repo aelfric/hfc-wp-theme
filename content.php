72a57abe2a3403f329c9373a665a1db18ea067b5
Frank Riccobono <F.Riccobono@gmail.com>
2014-05-01 10:09:57 -0400

    more cleaning up of repeated code.

--- content.php
+++ content.php
@@ -1,7 +1,20 @@
 <div class="news">
-   <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+   <?php 
+      if ( is_single() ) {
+         the_title( '<h1 class="entry-title">', '</h1>' );
+      } else {
+         the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+      }
+   ?>
    <span class="byline">Posted <?php the_time('F jS, Y') ?> by <?php the_author() ?></span>
-   <p><?php  the_content(); ?></p>
+   <?php  the_content(); ?>
 
-   <?php comments_template() ?>
+   <?php 
+      if (is_single()) {
+         comments_template();
+      } else { ?>
+      <span class="commentline"><a href="<?php comments_link(); ?>">Comments (<?php echo get_comments_number('%', '%', '%'); ?>)</a></span>
+      <?php
+      } 
+   ?>
 </div>
--- news.php
+++ news.php
@@ -1,38 +1,29 @@
 <?php
-	/*
-		Template Name: News Page
-	*/
-	
-	get_header();
-	include('slideshow.inc');
+   /*
+   Template Name: News Page
+   */
+
+   get_header();
+   include('slideshow.inc');
 ?>
 
 <div class="tealcor"></div>
-	<div class="teal">
-		<?php echo generate_teal_bar($post->id); ?>
-		<img id="ribbon" src="<?php echo get_stylesheet_directory_uri() ?>/img/ribbon.png" class="noselect"/>
-	</div>
+<div class="teal">
+   <?php echo generate_teal_bar($post->id); ?>
+   <img id="ribbon" src="<?php echo get_stylesheet_directory_uri() ?>/img/ribbon.png" class="noselect"/>
+</div>
 </div>
 <?php get_sidebar('news'); ?>
 <div class="box b640">
-		<!--- news loop stuff -->
-		<?php
-		$page_query = new WP_Query('posts_per_page=5'); 
-		get_header();
-		if (have_posts()) :
-		   while ($page_query->have_posts()) :
-			  $page_query->the_post();
-		?>
-		<div class="news">
-			<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-			<span class="byline">Posted <?php the_time('F jS, Y') ?> by <?php the_author() ?></span>
-			<p><?php  the_content(); ?></p>
-			<span class="commentline"><a href="<?php comments_link(); ?>">Comments (<?php echo get_comments_number('%', '%', '%'); ?>)</a></span>
-		</div>
-		<?php
-		   endwhile;
-		endif;
-		?>
-	</div>
+   <!--- news loop stuff -->
+   <?php
+      $page_query = new WP_Query('posts_per_page=5'); 
+      while ($page_query->have_posts()) {
+         $page_query->the_post();
+
+         get_template_part('content', get_post_format());
+      }
+   ?>
+</div>
 </div>
 <?php get_footer(); ?>
